be8a9bf1ebab1ea8096c0ee0c1837a2c10f7b8b5
MinsungKim <166286596+MinsungKimm@users.noreply.github.com>
2024-05-08 03:17:34 +0900

    Update App.js

--- parksstudents/src/App.js
+++ parksstudents/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+import { Mainpage } from "./pages/Mainpage";
+import { Statistics } from "./pages/Statistics";
+import { Login } from "./pages/Login";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/statistics" element={<Statistics />} />
+            <Route path="/mainpage" element={<Mainpage />} />
+        </Routes>    
     </div>
   );
 }
