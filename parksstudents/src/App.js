dc01c4ea28e73b281f6f8c81a7c2073b1a6e8f50
MinsungKim <166286596+MinsungKimm@users.noreply.github.com>
2024-05-10 00:35:11 +0900

    Update App.js

--- parksstudents/src/App.js
+++ parksstudents/src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import { Mainpage } from "./pages/Mainpage";
 import { Statistics } from "./pages/Statistics";
 import { Login } from "./pages/Login";
 
-import "./App.css";
-
 function App() {
   return (
-    <div className="App"> 
+    <BrowserRouter>
         <Routes>
+            <Route path="/" element={<Mainpage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/statistics" element={<Statistics />} />
-            <Route path="/mainpage" element={<Mainpage />} />
         </Routes>    
-    </div>
+    </BrowserRouter>
   );
 }
 
